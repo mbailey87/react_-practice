3f6073011e6cf09e99182142e77aa97c4b765291
MattBailey <mattbaileywork@gmail.com>
2025-08-25 20:29:46 -0600

    create base react ap

---
+++ moviedux/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Welcome to Moviedux</h1>
+      </header>
+      <footer>
+        <p className="footer">Footer Content Here</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
